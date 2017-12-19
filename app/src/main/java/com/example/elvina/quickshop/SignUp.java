e50b9856310df03935ab4aa26a3bb61dfeee91be
Timothy Oliver <oliverwakogi@live.com>
2017-12-19 16:30:54 +0300

    update repository

--- app/src/main/java/com/example/elvina/quickshop/SignUp.java
+++ app/src/main/java/com/example/elvina/quickshop/SignUp.java
@@ -2,14 +2,19 @@ package com.example.elvina.quickshop;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,12 +23,17 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SignUp extends AppCompatActivity implements View.OnClickListener{
     private EditText emailEditText;
     private EditText passwordEditText;
     private EditText confirmPasswordEditText;
     private TextView queryAccountPresenceTextView;
     private Button signUpButton;
+    private Spinner registerTypeSpinner;
     private ProgressDialog progressDialog;
     private FirebaseAuth mAuth;
 
@@ -40,6 +50,7 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
         confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
         queryAccountPresenceTextView = findViewById(R.id.queryAccountPresenceTextView);
         signUpButton = findViewById(R.id.signUpButton);
+        fillRegisterTypeSpinner();
         progressDialog = new ProgressDialog(this);
         queryAccountPresenceTextView.setOnClickListener(this);
         signUpButton.setOnClickListener(this);
@@ -95,5 +106,44 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
     public void onClick(View view) {
         if (signUpButton == view) registerUser();
         if (queryAccountPresenceTextView == view) startActivity(new Intent(this, Authenticate.class));
+    }
+
+    private void fillRegisterTypeSpinner(){
+        registerTypeSpinner = findViewById(R.id.registerTypeSpinner);
+        String[] registerTypes = new String[] { "select user type", "deliverer", "receiver", };
+        final List<String> registerTypeList = new ArrayList<>(Arrays.asList(registerTypes));
+
+        final ArrayAdapter<String> registerTypeSpinnerArrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, registerTypeList) {
+            @Override
+            public boolean isEnabled(int position) {
+                if(position == 0) return false;
+                else return true;
+            }
+
+            @Override
+            public View getDropDownView(int position, View concertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, concertView, parent);
+                TextView tv = (TextView) view;
+                if(position == 0) tv.setTextColor(Color.GRAY);
+                else tv.setTextColor(Color.BLACK);
+                return view;
+            }
+
+
+        };
+        registerTypeSpinnerArrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        registerTypeSpinner.setAdapter(registerTypeSpinnerArrayAdapter);
+        registerTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String registerTypeText = (String) adapterView.getItemAtPosition(i);
+                if(i > 0) Toast.makeText(getApplicationContext(), "Selected : " + registerTypeText, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/elvina/quickshop/profileModification.java
+++ app/src/main/java/com/example/elvina/quickshop/profileModification.java
@@ -1,7 +1,10 @@
 package com.example.elvina.quickshop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import static android.graphics.Color.parseColor;
 
 public class profileModification extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class profileModification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_modification);
+
+        getActionBar().setBackgroundDrawable(new ColorDrawable(parseColor("#3F3F3F")));
     }
 }
